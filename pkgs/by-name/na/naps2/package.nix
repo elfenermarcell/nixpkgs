3734fd1aca464d6ee9be7251a952d1ea6be34894
Marcell Tóth <elfenermarcell@gmail.com>
2025-02-16 14:14:59 +0100

    naps2: no longer broken on aarch64
    Co-authored-by: Fugi <me@fugi.dev>

--- pkgs/by-name/na/naps2/package.nix
+++ pkgs/by-name/na/naps2/package.nix
@@ -59,6 +59,5 @@ buildDotnetModule rec {
     maintainers = with lib.maintainers; [ eliandoran ];
     platforms = lib.platforms.linux;
     mainProgram = "naps2";
-    broken = stdenv.hostPlatform.isAarch64; # Google.Protobuf.Tools dependency fails to build.
   };
 }
